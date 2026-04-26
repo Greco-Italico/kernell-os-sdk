d15fe51cb352e3801bc5310c70426c8fe4e78ea1
Greco-Italico <conrrad.ads@gmail.com>
2026-04-25 19:25:37 -0500

    perf(worker): limit default max concurrency to 8 to prevent laptop starvation

--- kernell_os_sdk/worker/config.py
+++ kernell_os_sdk/worker/config.py
@@ -1,5 +1,5 @@
 import os
 
-MAX_CONCURRENCY = int(os.environ.get("MAX_CONCURRENCY", 20))
+MAX_CONCURRENCY = int(os.environ.get("MAX_CONCURRENCY", 8))
 EXECUTION_TIMEOUT = float(os.environ.get("EXECUTION_TIMEOUT", 2.0))
 AUTH_TOKEN = os.environ.get("AUTH_TOKEN", "SUPER_SECRET_INTERNAL_TOKEN")
